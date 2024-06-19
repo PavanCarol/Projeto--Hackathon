862699bedb3d163587d113dbf7580bc6f5406a94
Carolina Pavan <116219003+PavanCarol@users.noreply.github.com>
2024-06-19 12:39:59 -0300

    --register

--- PetService/src/app/app.routes.ts
+++ PetService/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { DialogRegisterComponent } from './components/dialog-register/dialog-register.component';
+import { RegisterComponent } from './components/register/register.component';
+
 export const routes: Routes = [
   {
     path: 'home',
@@ -13,6 +14,6 @@ export const routes: Routes = [
   },
   {
     path: 'cadastro',
-    component: DialogRegisterComponent,
+    component: RegisterComponent,
   },
 ];
--- PetService/src/app/components/register/register.component.ts
+++ PetService/src/app/components/register/register.component.ts
@@ -2,38 +2,36 @@ import { Component } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import {
   FormControl,
-  Validators,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
-
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { merge } from 'rxjs';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { DialogRegisterComponent } from '../components/dialog-forgot/dialog-register.component';
-import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { merge } from 'rxjs';
 @Component({
-  selector: 'app-login',
+  selector: 'app-register',
   standalone: true,
   imports: [
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
     MatIconModule,
   ],
-
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
 })
-export class LoginComponent {
+export class RegisterComponent {
   email = new FormControl('', [Validators.required, Validators.email]);
   hide = true;
   errorMessage = '';
-  constructor(public dialog: MatDialog) {
+  constructor() {
     merge(this.email.statusChanges, this.email.valueChanges)
       .pipe(takeUntilDestroyed())
       .subscribe(() => this.updateErrorMessage());
@@ -42,16 +40,6 @@ export class LoginComponent {
     this.hide = !this.hide;
     event.stopPropagation();
   }
-  openDialog(
-    enterAnimationDuration: string,
-    exitAnimationDuration: string
-  ): void {
-    this.dialog.open(DialogRegisterComponent, {
-      enterAnimationDuration,
-      exitAnimationDuration,
-    });
-  }
-
   updateErrorMessage() {
     if (this.email.hasError('required')) {
       this.errorMessage = 'Campo obrigatorio*';
