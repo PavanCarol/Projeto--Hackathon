87f8057b6cf987d94a6b707a7ff181c38b503129
Carolina Pavan <116219003+PavanCarol@users.noreply.github.com>
2024-07-26 18:24:32 -0300

    --Perfil/AlteracoesClinica

--- PetService/src/app/components/pet-service/perfil/perfil.component.ts
+++ PetService/src/app/components/pet-service/perfil/perfil.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule, FormsModule],
   templateUrl: './perfil.component.html',
-  styleUrl: './perfil.component.scss'
+  styleUrl: './perfil.component.scss',
 })
 export class PerfilComponent {
+  isDisabled = true;
+  profile = {
+    name: '',
+    email: '',
+    password: '',
+  };
 
+  toggleEdit() {
+    this.isDisabled = !this.isDisabled;
+  }
+  saveChanges() {
+    // Lógica para salvar as mudanças
+    console.log('Changes saved:', this.profile);
+    this.toggleEdit(); // Desabilitar novamente os inputs após salvar
+  }
 }
